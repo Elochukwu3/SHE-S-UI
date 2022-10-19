879828d75b603dd3368a8a108d279ba43ed7a1dc
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-19 18:26:58 +0100

    save styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import NavBar from './components/NavBar';
+import SideDisplay from './components/SideDisplay';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <section>
+        <SideDisplay/>
+      </section>
       
     </div>
   );
---
+++ src/components/SideDisplay.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function SideDisplay() {
+  return (
+    <div className='sideArticle' style={{position: 'relative'}}>
+        <p>Fresh Winter Collection</p>
+        <h2>make your</h2>
+        <h2>look more</h2>
+        <div className='perfect'>perfect</div>
+        <div>
+            <span>fashion</span>
+            <span>Trend</span>
+            <span>Lifestyle</span>
+        </div>
+        <button>
+            shopping collections
+        </button>
+        
+    </div>
+  )
+}
