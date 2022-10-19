9cdd6a72563bb510956770b877bfb558e65041f9
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-19 16:22:24 +0100

    save changes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function () {
+  return (
+   
+      <header>
+        <p className="headerName" data-end ='tudio'>
+            SHE'S
+        </p>
+        <ul className="menuList menu">
+            <li>Men</li>
+            <li>Women</li>
+            <li className="menuHover">Accessories <span>new</span></li>
+            <li>Sales</li>
+            <li>Winber</li>
+        </ul>
+        <ul className="menuList carts">
+            <li><img src="/search.png" alt="" className="search"/></li>
+            <li>Cart</li>
+            <li><img src="/shopping-cart.png" alt="" className="cart"/></li>
+        </ul>
+      </header>
+ 
+  );
+}
