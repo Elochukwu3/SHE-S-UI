5fa73f66fa75177b9a4204a77ae27a205f7fe032
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-20 06:24:42 +0100

    savde

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <section>
+      <section className='sideContainer'>
         <SideDisplay/>
       </section>
       
