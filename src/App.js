9f5057d5c399a9900bd2f91ffe34aea20229a2df
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-20 13:55:16 +0100

    saved all

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-
-import './App.css';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import { Route, Routes } from 'react-router-dom';
-import WomenPage from './components/WomenPage';
-import Accessories from './components/Accessories ';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import WomenPage from "./components/WomenPage";
+import Accessories from "./components/Accessories ";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='women' element={<WomenPage/>}/>
-        <Route path='accessories' element={<Accessories/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="women" element={<WomenPage />} />
+        <Route path="accessories" element={<Accessories />} />
       </Routes>
-
-      
     </div>
   );
 }
--- src/components/Accessories .js
+++ src/components/Accessories .js
@@ -1,30 +1,64 @@
-import { Faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
 
-export default function Accessories () {
+const imgSize = {
+  width: "11rem",
+  height: "10rem",
+  borderRadius: ".9rem",
+  ObjectFit: "cover",
+};
+export default function Accessories() {
   return (
     <>
-  <h1 style={{textAlign: 'center', color: '#fff', paddingBottom: '1rem'}}>Colllections</h1>
-   <div style={{ display: "flex", justifyContent:'space-around', flexWrap: 'wrap', gap:'.1rem' }} className='women'>
+      <h1 style={{ textAlign: "center", color: "#fff", paddingBottom: "1rem" }}>
+        Colllections
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+          gap: ".1rem",
+        }}
+        className="women"
+      >
         <div>
-            <img src={faker.image.sports()} alt={'randomEmail'} style={imgSize}/>
-            <p>{faker.hacker.noun()}{faker.helpers.mustache()}</p>
+          <img
+            src={faker.image.business()}
+            alt={"randomEmail"}
+            style={imgSize}
+          />
+          <p>
+            {faker.hacker.noun()}
+            {faker.helpers.mustache()}
+          </p>
         </div>
-        <div><img src={faker.image.technics()} alt={'rando'} style={imgSize}/>
-        <p>{faker.commerce.department()}</p>
+        <div>
+          <img src={faker.image.technics()} alt={"rando"} style={imgSize} />
+          <p>{faker.commerce.department()}</p>
         </div>
-        <div><img src={faker.image.nightlife()} alt={'rando'} style={imgSize}/>
-        <p>{faker.name.lastName() } John</p>
+        <div>
+          <img src={faker.image.nightlife()} alt={"rando"} style={imgSize} />
+          <p>{faker.name.lastName()} John</p>
         </div>
-        <div><img src={faker.image.people() } alt={'rando'} style={imgSize}/>
-        <p>{faker.name.lastName() }  {faker.hacker.noun()}</p></div>
-        <div><img src={faker.image.nightlife()} alt={'rando'} style={imgSize}/>
-        <p>{faker.name.lastName() }  {faker.hacker.noun()}</p>
+        <div>
+          <img src={faker.image.business()} alt={"rando"} style={imgSize} />
+          <p>
+            {faker.name.lastName()} {faker.hacker.noun()}
+          </p>
         </div>
-        <div><img src={faker.image.business() } alt={'rando'} style={imgSize}/>
-        <p>{faker.name.lastName() } {faker.hacker.noun()}</p>
+        <div>
+          <img src={faker.image.nightlife()} alt={"rando"} style={imgSize} />
+          <p>
+            {faker.name.lastName()} {faker.hacker.noun()}
+          </p>
         </div>
-        
-  </div>
-  </>
-  )
+        <div>
+          <img src={faker.image.business()} alt={"rando"} style={imgSize} />
+          <p>
+            {faker.name.lastName()} {faker.hacker.noun()}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
